11aed79c189ae45474c434f302273f72f08ad65c
muhannadpv786 <p.vmuhannad786@gmail.com>
2025-07-22 15:47:58 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "muhannadmegaproject"
+    bucket         = "muhannadmegaproject1"
     key            = "eks-cluster/terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true
